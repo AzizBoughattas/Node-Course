b13994180514eecf342ad98df589255acecb8342
AzizBoughattas <boughattas.medaziz@esprit.tn>
2022-06-09 18:19:54 +0100

    Setup app for Heroku

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -18,7 +18,7 @@ weatherForm.addEventListener('submit' , (event) => {
     event.preventDefault()
     const location= searchElement.value
     console.log(location)
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
             messageOne.textContent = data.error
