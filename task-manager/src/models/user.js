12364a0e8312296c110f7d32cb9291d9263ec077
AzizBoughattas <boughattas.medaziz@esprit.tn>
2022-06-13 16:25:31 +0100

    sending emails

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -75,7 +75,7 @@ userSchema.methods.toJSON = function ()  {
 
 userSchema.methods.generateAuthToken = async function () {
    // const user = this
-   const token = jwt.sign({_id : this._id.toString()} , 'thisismynewcourse')
+   const token = jwt.sign({_id : this._id.toString()} , process.env.JWT_SECRET)
 
    this.tokens = this.tokens.concat({token : token})
    await this.save()
